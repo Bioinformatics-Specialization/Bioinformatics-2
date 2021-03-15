1fcf5113c6396196eddc6740d73845bfbe0534b3
khaeuk <khaeuk@gmail.com>
2021-03-14 20:43:37 -0700

    Able to find small sub cycle within the graph.

--- week1/eulerian_cycle.py
+++ week1/eulerian_cycle.py
@@ -1,12 +1,35 @@
 import random
 import os
 import sys
+import random
 import w1lib
+from copy import deepcopy
 DATASET_DIR = os.path.join(os.getcwd(), 'datasets')
 
+def find_cycle(node, adjacency_dict) : 
+    graph_dict = deepcopy(adjacency_dict)
+    cycle = [node]
+
+    while True :
+        start_node = cycle[0] if len(cycle) == 1 else next_node
+        next_nodes = graph_dict[start_node]
+        
+        if 0 < len(next_nodes) :
+            next_node = random.choice(next_nodes)
+            cycle.append(next_node)
+            graph_dict[start_node].remove(next_node)
+        else :
+            if node in adjacency_dict[start_node] : cycle.append(node)
+            return cycle
 
 def eulerCycle(adjacency_dict) :
+    # random.seed(33)
+    available_nodes = list(adjacency_dict.keys())
+    start_node = random.choice(available_nodes)
 
+    # Find cycle
+    cycle = find_cycle(start_node, adjacency_dict)  
+    print("Cycle : {}".format("->".join(cycle)))
 
 def main() :
     util = w1lib.Week1Library()
@@ -45,10 +68,13 @@ def main() :
 
     for row in rows : 
         row = row.replace(" ", "").strip().split("->")
-        adjacency_dict[int(row[0])] = [int(r) for r in row[1].split(",")]
+        adjacency_dict[row[0]] = [r for r in row[1].split(",")]
     
+    for k, v in adjacency_dict.items() :
+        print("{} -> {}".format(k, v))
+    print("_____________________________")
     cycle = eulerCycle(adjacency_dict)
-    print(cycle)
+    
 
 if __name__ == "__main__":
     main()
