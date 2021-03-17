8c7761074bb5996982448c5f4ee23f83ef42fa89
khaeuk <khaeuk@gmail.com>
2021-03-16 17:29:43 -0700

    Finished genomePath, but need to clean up a little.

---
+++ week1/genome_path.py
@@ -0,0 +1,53 @@
+import random
+import os
+import sys
+import random
+import w1lib
+DATASET_DIR = os.path.join(os.getcwd(), 'datasets')
+
+
+def genomePath(kmers) :
+    reconstructed_str = kmers[0]
+
+    for kmer in kmers[1:] :
+        reconstructed_str += kmer[-1]
+    
+    return reconstructed_str
+
+def main() :
+    util = w1lib.Week1Library()
+    description = '''\
+        Given collection of kmers, it will reconstruct the full genome.
+
+        Input File format :
+        ---------------------------------------
+        ACCGA
+        CCGAA
+        CGAAG
+        GAAGC
+        AAGCT
+
+        Expected output :
+        ---------------------------------------
+        ACCGAAGCT
+        '''
+    args = util.create_parser(__file__, description)
+
+    dataset_path = "{}/{}_dataset.txt".format(DATASET_DIR, os.path.splitext(sys.argv[0])[0])
+
+    # Default to the dataset folder, if not provided
+    if not args.file : args.file = dataset_path
+
+    kmers = []
+
+    # Read from dataset file
+    with open(args.file, 'r') as f :
+        contents = f.readlines()
+        [kmers.append(_.strip()) for _ in contents]
+
+    path = genomePath(kmers)
+    print(path)
+
+
+if __name__ == "__main__":
+    main()
